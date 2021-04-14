489d0b52fda2e018e0fca9c0317aacc721988103
Ermesson Lima <ermessonlimadossantos@hotmail.com>
2021-04-13 23:04:55 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty Hall
+      <p>2º P. Estatística e Prob.</p>
+    </h1>
+       <h1>Encontre a porta com a nota 10 em Estatística e Probabilidade
+    </h1>
+    <div class="form">
+      <div v-if="!started">
+        <label for="portsAmount"> Quantas portas? </label>
+        <input
+          type="text"
+          id="portsAmount"
+          size="3"
+          v-model.number="portsAmount"
+        />
+      </div>
+      <div v-if="!started">
+        <label for="selectedPort"> Qual das porta é a premiada? </label>
+        <input
+          type="text"
+          id="selectedPort"
+          size="3"
+          v-model.number="selectedPort"
+        />
+      </div>
+ 
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started = false">Reiniciar</button>
+    </div>
+
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Door :hasGift="i === selectedPort" :number="i" />
+      </div>
+    </div>
+   <p>Aluno: Ermesson Lima dos Santos
+    </p>
+  </div>
+  
+</template>
+
+<script>
+import Door from './components/Door'
+export default {
+  name: "App",
+  components: {
+    Door
+  },
+  data: function () {
+    return {
+      started: false,
+      portsAmount: 3,
+      selectedPort: null,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#app h1 {
+
+  padding: 20px;
+  margin-bottom: 60px;
+}
+body {
+  background: linear-gradient(to right, #a8ff78, #78ffd6);
+}
+
+.form {
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/Gift.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="gift">
+      <div class="gift-tie-1"></div>
+      <div class="gift-tie-2">
+          <p class="nota">10</p>
+          <p class="nota2">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups
+              
+          </p>
+      </div>
+  
+      <div class="gift-body"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Gift'
+}
+</script>
+
+<style>
+.gift {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 10;
+    margin-bottom: 80px;
+}
+
+.gift-body {
+    width: 90px;
+    height: 130px;
+    background-color: #ffffff;
+}
+.gift-tie-1 {
+    position: absolute;
+    width: 15px;
+    height: 85px;
+   
+}
+.nota {
+    font-size: 20px;
+    color: red;
+    margin-top: 10px;
+
+}
+
+.nota2 {
+    font-size: 5px;
+    color: black;
+    margin-top: 10px;
+
+}
+.gift-tie-2 {
+    justify-content: center;
+    align-self: center;
+    align-items: center;
+    text-align: center;
+    position: absolute;
+    top:5px;
+    width: 40px;
+    height: 40px;
+  border-radius: 20px;
+   border: 2px solid red;
+  
+}
+</style>
